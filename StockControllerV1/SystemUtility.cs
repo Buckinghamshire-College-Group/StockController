27351166fd17555bf6e16c4b7b4a1f7f51e6c0b8
Darren Hannay <dmhsoftad01@outlook.com>
2021-06-20 16:02:32 +0100

    added connection string to connect system to database

---
+++ StockControllerV1/SystemUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockControllerV1
+{
+	public class SystemUtility
+	{
+		public static string GetDatabaseConnection(int DatabaseType)
+		{
+			if (DatabaseType == 1)
+			{
+				return @"Initial Catalog=DMHStockMasterV2021;Data Source=.\\SQLEXPRESS;Persist Security=False;Integrated Security=true;";
+			}
+			else if (DatabaseType == 2)
+			{
+				return @"Initial Catalog=DMHStockv5;Data Source=.\\SQLEXPRESS;Persist Security=False;Integrated Security=true;";
+			}
+			else if (DatabaseType == 3)
+			{
+				return @"Initial Catalog=master;Data Source=.\\SQLEXPRESS;Persist Security=False;Integrated Security=true;";
+			}
+			else
+			{
+				return @"Initial Catalog=master;Data Source=.\\SQLEXPRESS;Persist Security=False;Integrated Security=true;";
+			}
+		}
+	}
+}
