47c07558c89062ee1790576bb8e9a01bff3b2d94
NeuralVentaTech <61027383+Dhannay2020@users.noreply.github.com>
2025-09-28 13:59:32 +0100

    Start of project Step 1
    Step 1

---
+++ WarehouseSMS/WarehouseSMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WarehouseSMS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
